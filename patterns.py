1bbe62afe34ce11bf29025fa00eed8c39e313b02
infinitecoder1729 <77016507+infinitecoder1729@users.noreply.github.com>
2021-07-07 00:30:09 +0530

    Create patterns.py

---
+++ patterns.py
@@ -0,0 +1,91 @@
+''' * 
+    * * 
+    * * * 
+    * * * * 
+    * * * * * '''
+n=int(input("Enter Number of Rows : "))
+for i in range(0, n):
+     for j in range(0, i+1):
+          print("*",end= " ")
+     print()
+   
+'''      * 
+        * * 
+       * * * 
+      * * * * 
+     * * * * * '''
+n=int(input("Enter Number of Rows : "))
+c = n - 1
+for i in range(0, n):
+     for t in range(0, c):
+          print(end=" ")
+     c = c - 1
+     for t in range(0, i+1):
+          print("* ", end="")
+     print(" ")
+    
+''' 1 
+    1 2 
+    1 2 3 
+    1 2 3 4 
+    1 2 3 4 5 '''
+num_input = int(input("Enter the Starting Number : "))
+n=int(input("Enter Number of Rows : "))
+for i in range(0, n):
+     num=num_input
+     for j in range(0, i+1):
+          print(num, end=" ")
+          num = num + 1
+     print()
+    
+''' A 
+    A B 
+    A B C 
+    A B C D 
+    A B C D E '''
+alph=input("Enter the Starting Letter : ")
+num_user = ord(alph)
+n=int(input("Enter Number of Rows : "))
+for i in range(0, n):
+     num=num_user
+     for j in range(0, i+1):
+          ch = chr(num)
+          print(ch, end=" ")
+          num = num + 1
+     print()
+  
+''' 5 4 3 2 1 
+    4 3 2 1 
+    3 2 1 
+    2 1 
+    1        '''
+n = int(input("Enter Number of Rows : "))
+for i in range(0, n + 1):
+    for j in range(n - i, 0, -1):
+        print(j, end=' ')
+    print()
+    
+''' 1 2 3 4 5 
+    2 2 3 4 5 
+    3 3 3 4 5 
+    4 4 4 4 5 
+    5 5 5 5 5  '''
+n = int(input("Enter the Number of Columns/Rows : "))
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        if j <= i:
+            print(i, end=' ')
+        else:
+            print(j, end=' ')
+    print()
+''' *****
+    ****
+    ***
+    **
+    *   '''
+n=int(input("What number of '*' do you want to begin with ? "))
+for i in range (n,0,-1):
+    for a in range (1,i+1):
+        print("*", end="")
+    else :
+        print()
